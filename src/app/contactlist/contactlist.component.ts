1cc38c2567ed486fa010f6b8db25b89daf48e610
yulitandria <yulitandria@gmail.com>
2020-01-29 16:50:42 +0700

    sorting failed

--- src/app/contactlist/contactlist.component.ts
+++ src/app/contactlist/contactlist.component.ts
@@ -1,10 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive, Input, Output, ViewChildren, QueryList, EventEmitter } from '@angular/core';
 import { Contact } from '../model/contact.model';
 import { Subscription } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ContactService } from '../service/app.service';
 import { EventEmitterService } from '../service/eventemtiier.service';
 import { EditableContact } from '../model/editablecontact.model';
+
+//--------------------------------------------------
+export type SortDirection = 'asc' | 'desc' | '';
+const rotate: {[key: string]: SortDirection} = { 'asc': 'desc', 'desc': '', '': 'asc' };
+export const compare = (v1, v2) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+
+export interface SortEvent {
+  column: string;
+  direction: SortDirection;
+}
+
+@Directive({
+  selector: 'th[sortable]',
+  host: {
+    '[class.asc]': 'direction === "asc"',
+    '[class.desc]': 'direction === "desc"',
+    '(click)': 'rotate()'
+  }
+})
+export class NgbdSortableHeader {
+
+  @Input() sortable: string;
+  @Input() direction: SortDirection = '';
+  @Output() sort = new EventEmitter<SortEvent>();
+
+  rotate() {
+    this.direction = rotate[this.direction];
+    this.sort.emit({column: this.sortable, direction: this.direction});
+  }
+}
+
+//--------------------------------------------------
 
 @Component({
   selector: 'app-contactlist',
@@ -92,4 +124,29 @@ export class ContactlistComponent implements OnInit {
     };
     this.eventEmitterService.onShowDialogButtonClick(newContact);
   }
+
+  //---------------------------------------------------------------------------
+
+  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+  onSort({column, direction}: SortEvent) {
+
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    // sorting countries
+    if (direction === '') {
+      this.loadedContacts  = this.loadedContacts ;
+    } else {
+      this.loadedContacts = [...this.loadedContacts].sort((a, b) => {
+        const res = compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      });
+    }
+  }
 }
+
+
